aa3561c8cb69f587c08d4ab6443765df48363886
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-04 01:35:28 +0900

    出力形式とπの精度不足

--- ITP_1_4_B.cpp
+++ ITP_1_4_B.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
     double r, m, l;
     cin >> r;
-    m = r*r*3.14;
-    l = 2.0*r*3.14;
-    cout << fixed << setprecision(6) << m;
+    m = r*r*3.14159265;
+    l = 2.0*r*3.14159265;
+    cout << fixed << setprecision(6) << m << " ";
     cout << fixed << setprecision(6) << l << endl;
     return 0;
 }
