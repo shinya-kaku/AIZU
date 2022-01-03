7d93c0c3480ad5200cd7c9689a3a876b882e71a6
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-04 01:19:08 +0900

    小数点の取り扱い

---
+++ ITP_1_4_A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    int r, d;
+    double f;
+    cin >> a >> b;
+    r = a/b;
+    d = a%b;
+    f = a/b;
+    cout << r << " " << d << " " << f << endl;
+    return 0;
+}
