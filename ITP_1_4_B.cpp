bb34c5bd6db58769bb5a27b096447377351f572f
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-04 01:32:38 +0900

    円の面積と演習の出力

---
+++ ITP_1_4_B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double r, m, l;
+    cin >> r;
+    m = r*r*3.14;
+    l = 2.0*r*3.14;
+    cout << fixed << setprecision(6) << m;
+    cout << fixed << setprecision(6) << l << endl;
+    return 0;
+}
