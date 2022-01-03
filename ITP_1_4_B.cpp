c2f2b10b0de280908b9b009bb184df10c04e188b
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-04 01:40:08 +0900

    精度が足りていなかったので事前に定義されている数を使う形式に改める

--- ITP_1_4_B.cpp
+++ ITP_1_4_B.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main(){
     double r, m, l;
     cin >> r;
-    m = r*r*3.14159265;
-    l = 2.0*r*3.14159265;
+    m = r*r*M_PI;
+    l = 2.0*r*M_PI;
     cout << fixed << setprecision(6) << m << " ";
     cout << fixed << setprecision(6) << l << endl;
     return 0;
