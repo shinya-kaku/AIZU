58f23ee9e86b417f8392df16526d8ea30a04e242
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-05 23:30:47 +0900

    boolで管理

---
+++ ITP_1_5_C.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int H, W;
+    bool check = true;
+    do {
+        cin >> H >> W;
+        if (H == 0 and W == 0){
+            break;
+        }
+        for (int i=0; i<H; i++){
+            for (int j=0; j<W; j++){
+                if (check == true){
+                    cout << "#";
+                    check = false;
+                } else {
+                    cout << ".";
+                    check = true;
+                }
+            }
+            cout << endl;
+        }
+        cout << endl;
+    } while (H != 0);
+
+    return 0;
+}
