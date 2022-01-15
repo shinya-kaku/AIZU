b1efa0f51da2a00bf14af782389671130240ea3a
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-16 02:55:36 +0900

    小数点の指定

--- ITP_1_10_A.cpp
+++ ITP_1_10_A.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
 //Distance
 
+ //P1(x1, y1), P2(x2, y2)
+
+int main(){
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2; 
+    //Pythagorean theorem c^2 = a^2 + b^2
+    cout << fixed;
+    cout << setprecision(8) << sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))  << endl; 
+    return 0;
+}
