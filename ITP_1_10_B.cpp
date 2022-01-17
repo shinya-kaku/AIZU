0702370da5d7a62b523a43b68703bbe914402021
shinya kaku <59046606+shinya-kaku@users.noreply.github.com>
2022-01-18 00:12:01 +0900

    3辺目の求め方の誤り。

--- ITP_1_10_B.cpp
+++ ITP_1_10_B.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
 using namespace std;
 
 //Triangle
 
 int main() {
     double a, b, C;
+    double rad;
     cin >> a >> b >> C;
-    cout << a*b*sin(C) << endl;
-    cout << a + b + sqrt((a*a) + (b*b)) << endl;
-    cout << b*sin(C) << endl;
+    //cout << a << b << C << endl;
+    rad = C * (M_PI/180);
+    //cout << sin(rad) << endl;
+    cout << std::fixed;
+    cout << std::setprecision(11) << 0.5*a*b*sin(rad) << endl;
+    cout << std::setprecision(11)  << a + b + sqrt((a*a) + (b*b) - (2.0*a*b*cos(rad))) << endl;
+    cout << std::setprecision(11)  << b*sin(rad) << endl;
 
     return 0;
 }
